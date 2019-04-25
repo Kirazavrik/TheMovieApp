3dc9a7e68128ec87e37e584b7688405b2a36b1c6
Kirabot <wr30mg@gmail.com>
2019-04-25 10:46:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/themovieapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.themovieapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
